e52252673c3a14a7e51fd3550e934ff55cc8af9f
Juhyung990122 <harukawa99@naver.com>
2021-02-24 01:25:08 +0900

    Init project

---
+++ src/main/java/com/keyduck/KeyduckBackApplication.java
@@ -0,0 +1,13 @@
+package com.keyduck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeyduckBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeyduckBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/keyduck/KeyduckBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keyduck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeyduckBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
