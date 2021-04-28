d3b2a99eb335a0181e52647be242cd6d222a600f
Juhyung990122 <harukawa99@naver.com>
2021-04-28 20:59:36 +0900

    Set Keyboard's Testcode

--- src/main/java/com/keyduck/keyboard/controller/KeyboardController.java
+++ src/main/java/com/keyduck/keyboard/controller/KeyboardController.java
@@ -6,9 +6,7 @@ import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/v1")
@@ -21,7 +19,11 @@ public class KeyboardController {
 
     @GetMapping("/keyboard")
     public ResponseEntity<?> searchKeyboard(){
+        return new ResponseEntity<>(responseService.getSuccessResult(), HttpStatus.OK);
+    }
 
+    @PostMapping("/keyboard/add")
+    public ResponseEntity<?> addKeyboard(){
         return new ResponseEntity<>(responseService.getSuccessResult(), HttpStatus.OK);
     }
 
--- src/test/java/com/keyduck/keyboard/controller/KeyboardControllerTest.java
+++ src/test/java/com/keyduck/keyboard/controller/KeyboardControllerTest.java
@@ -3,9 +3,14 @@ package com.keyduck.keyboard.controller;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class KeyboardControllerTest {
     @Autowired
@@ -23,7 +28,59 @@ public class KeyboardControllerTest {
     }
 
     @Test
-    public void Keyboard_추가() {
+    public void Keyboard_전체조회() throws Exception{
+        mvc.perform(get("v1/keyboards/")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+
+    @Test
+    public void Keyboard_디테일조회() throws Exception{
+        mvc.perform(get("v1/keyboards/테스트키보드")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+
+    @Test
+    public void Keyboard_추가() throws Exception {
+        String successData = "{\"model\": \"테스트키보드\", \"brand\": \"ABKO\", \"connect\": \"유선\", \"switchBrand\": \"오테뮤\", \"switchColor\": \"청축\", \"hotswap\": \"○\", \"price\": 0, \"keycap\": \"이중사출 키캡\", \"keycapImprint\": \"한글 정각\", \"keycapProfile\": \"default\", \"led\": \"레인보우 백라이트\", \"arrangement\": \"104\", \"weight\": \"1050\", \"cable\": \"○\", \"photo\": \"default\"}";
+        mvc.perform(post("/v1/keyboards/add")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(successData))
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+
+    @Test
+    public void Keyboard_삭제() throws Exception{
+        String successData = "{\"model\":\"테스트키보드\"}";
+        mvc.perform(delete("/v1/keyboards/delete")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(successData))
+                .andExpect(status().isOk())
+                .andDo(print());
+
 
     }
+
+    @Test
+    public void Keyboard_검색() throws Exception{
+        String successDataKeyword = "{\"keyword\" : [\"brand\":\"로지텍\"] }";
+        String  successDataKeywords = "{\"keyword\" : [\"brand\",\"로지텍\",\"connect\":\"유선\"]}";
+
+        mvc.perform(post("/v1/keyboards/")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(successDataKeyword))
+                .andExpect(status().isOk())
+                .andDo(print());
+
+        mvc.perform(post("/v1/keyboards/")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(successDataKeywords))
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+
 }
