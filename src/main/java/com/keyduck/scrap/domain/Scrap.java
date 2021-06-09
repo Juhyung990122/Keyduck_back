9f99ddf7a9665bccf991f36e0c60e1de5b447bf5
Juhyung990122 <harukawa99@naver.com>
2021-06-09 18:35:20 +0900

    Edit Scrap relation

--- src/main/java/com/keyduck/scrap/domain/Scrap.java
+++ src/main/java/com/keyduck/scrap/domain/Scrap.java
@@ -1,24 +1,28 @@
 package com.keyduck.scrap.domain;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.keyduck.keyboard.domain.Keyboard;
 import com.keyduck.member.domain.Member;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 import javax.persistence.*;
+import java.io.Serializable;
 
 @Entity
 @NoArgsConstructor
+@Table(name = "scrap")
 @IdClass(ScrapId.class)
-public class Scrap {
+public class Scrap implements Serializable {
 
     @Id
     @ManyToOne
-    @JoinColumn(name="memberId")
+    @JoinColumn(name="member_id")
     private Member memberId;
 
     @Id
     @ManyToOne
-    @JoinColumn(name="keyboardId")
+    @JoinColumn(name="keyboard_id")
     private Keyboard keyboardId;
 
 }
