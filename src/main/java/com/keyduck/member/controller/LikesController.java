3ebe6666f558e94e51eb458cbb183e509a770a35
Juhyung990122 <harukawa99@naver.com>
2021-08-23 23:15:10 +0900

    [docs] 좋아요 기능 스웨거 설명 추가

--- src/main/java/com/keyduck/member/controller/LikesController.java
+++ src/main/java/com/keyduck/member/controller/LikesController.java
@@ -4,6 +4,8 @@ import com.keyduck.keyboard.dto.SimpleKeyboardDto;
 import com.keyduck.member.service.LikesService;
 import com.keyduck.result.ResponseService;
 import com.keyduck.result.SingleResult;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +16,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "/v1")
+@Api(tags = "Likes", value = "KeyduckController v1")
 public class LikesController {
     private final LikesService likesService;
     private final ResponseService responseService;
@@ -24,6 +27,7 @@ public class LikesController {
     }
 
     @GetMapping("/{memId}/likes")
+    @ApiOperation(value = "멤버별 좋아요 조회", notes = "특정 멤버가 좋아요한 키보드들을 조회합니다.")
     public ResponseEntity<SingleResult<List<SimpleKeyboardDto>>> getLikeKeyboards(@PathVariable Long memId){
         List<SimpleKeyboardDto> result = likesService.getLikes(memId);
         return ResponseEntity
@@ -32,6 +36,7 @@ public class LikesController {
     }
 
     @PostMapping("/{memId}/likes")
+    @ApiOperation(value = "멤버별 좋아요 추가", notes = "특정 멤버가 좋아요한 키보드를 추가합니다.")
     public ResponseEntity<SingleResult<String>> addLikeKeyboards(@PathVariable Long memId, @RequestBody HashMap<String,Long> keyboardId){
         String result = likesService.addLikes(memId,keyboardId.get("keyboardId"));
         return ResponseEntity
@@ -40,6 +45,7 @@ public class LikesController {
     }
 
     @DeleteMapping("/{memId}/likes")
+    @ApiOperation(value = "멤버별 좋아요 취소", notes = "특정 멤버가 좋아요한 키보드들을 삭제합니다.")
     public ResponseEntity<SingleResult<String>> deleteLikeKeyboards(@PathVariable Long memId, @RequestBody HashMap<String,Long> keyboardId){
         String result = likesService.deleteLikes(memId,keyboardId.get("keyboardId"));
         return ResponseEntity
