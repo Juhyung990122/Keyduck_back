057cc85f0637f8bf611397171ea46e7a43e077d6
Juhyung990122 <harukawa99@naver.com>
2021-11-12 00:22:26 +0900

    [Refactor] 멤버 컨트롤러 테스트코드 리팩토링 완료

--- src/test/java/com/keyduck/member/controller/MemberControllerTest.java
+++ src/test/java/com/keyduck/member/controller/MemberControllerTest.java
@@ -1,37 +1,29 @@
 package com.keyduck.member.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.keyduck.keyboard.domain.Keyboard;
-import com.keyduck.keyboard.repository.KeyboardRepository;
 import com.keyduck.member.domain.Member;
 import com.keyduck.member.domain.MemberRole;
 import com.keyduck.member.domain.MemberType;
 import com.keyduck.member.domain.RequestMember;
 import com.keyduck.member.repository.MemberRepository;
 
-import org.elasticsearch.monitor.os.OsStats;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.EnableLoadTimeWeaving;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.security.test.context.support.WithMockUser;
 
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import java.util.Optional;
@@ -39,8 +31,6 @@ import java.util.Optional;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.junit.Assert.assertThat;
 
 @RunWith(SpringRunner.class)
@@ -72,22 +62,26 @@ public class MemberControllerTest {
 
     @Test
     @WithMockUser(username="admin", roles = "ADMIN")
-    public void getAllMembers() throws Exception {
-        mvc.perform(get("/v1/members")
+    public void 전체회원조회_성공() throws Exception {
+        //given
+        //when
+        MockHttpServletResponse response = mvc.perform(get("/v1/members")
                 .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andDo(print());
+                .andReturn()
+                .getResponse();
+        //then
+        assertThat(response.getStatus(),is(HttpStatus.OK.value()));
     }
 
     @Test
-    @WithMockUser(username="admin", roles = "ADMIN")
-    public void getMemberDetail() throws Exception{
+    @WithMockUser(username="admin", roles = "USER")
+    public void 개별회원조회_성공() throws Exception{
         //given
         Member detailMember = Member.MemberBuilder()
                 .email("detailTest@naver.com")
                 .build();
         detailMember.setMemId(1L);
-        when(memberRepository.save(any())).thenReturn(detailMember);
+        when(memberRepository.findById(any())).thenReturn(Optional.of(detailMember));
         //when
         MockHttpServletResponse response = mvc.perform(get("/v1/members/" + detailMember.getMemId().toString())
                 .contentType(MediaType.APPLICATION_JSON))
@@ -100,14 +94,14 @@ public class MemberControllerTest {
     @Test
     public void 회원가입_성공() throws Exception {
         //given
-        RequestMember rightMember = new RequestMember();
-        rightMember.setEmail("rightMember@naver.com");
-        rightMember.setPassword("1212");
+        RequestMember request = new RequestMember();
+        request.setEmail("rightMember@naver.com");
+        request.setPassword("1212");
 
         //when
         MockHttpServletResponse response = mvc.perform(post("/v1/signup")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(mapper.writeValueAsString(rightMember)))
+                .content(mapper.writeValueAsString(request)))
                 .andReturn()
                 .getResponse();
 
@@ -120,11 +114,11 @@ public class MemberControllerTest {
     @Test
     public void 회원가입_실패_유효성검사() throws Exception {
         //given
-        RequestMember wrongMember = new RequestMember();
+        RequestMember request = new RequestMember();
         //when
         MockHttpServletResponse response = mvc.perform(post("/v1/signup")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(mapper.writeValueAsString(wrongMember)))
+                .content(mapper.writeValueAsString(request)))
                 .andReturn()
                 .getResponse();
         //then
@@ -142,10 +136,15 @@ public class MemberControllerTest {
                 .build();
         when(memberRepository.findByEmail("signintest@naver.com"))
                 .thenReturn(Optional.ofNullable(member));
+
+        RequestMember request = new RequestMember();
+        request.setEmail("signintest@naver.com");
+        request.setPassword("1212");
+
         //when
         MockHttpServletResponse response = mvc.perform(post("/v1/login")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content("{\"email\":\"signintest@naver.com\",\"password\":\"1212\"}"))
+                .content(mapper.writeValueAsString(request)))
                 .andReturn()
                 .getResponse();
         //then
@@ -153,8 +152,9 @@ public class MemberControllerTest {
 
     }
     @Test
-    @WithMockUser(username="admin", roles = "ADMIN")
-    public void leave() throws Exception{
+    @WithMockUser(username="leavetest", roles = "USER")
+    public void 회원탈퇴_성공() throws Exception{
+        //given
         Member member = Member.MemberBuilder()
                 .nickname("test")
                 .email("leavetest@naver.com")
@@ -162,10 +162,19 @@ public class MemberControllerTest {
                 .role(MemberRole.ADMIN)
                 .type(MemberType.Keyduck)
                 .build();
-        memberRepository.save(member);
-        mvc.perform(delete("/v1/members/leave")
+        when(memberRepository.findByEmail(any())).thenReturn(Optional.ofNullable(member));
+        RequestMember request = new RequestMember();
+        request.setEmail("leavetest@naver.com");
+
+        //when
+        MockHttpServletResponse response = mvc.perform(delete("/v1/members/leave")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content("{\"email\":"+"\""+member.getEmail()+"\"}"))
-                .andExpect(status().isOk());
+                .content(mapper.writeValueAsString(request)))
+                .andReturn()
+                .getResponse();
+
+        //then
+        assertThat(response.getStatus(),is(HttpStatus.OK.value()));
+        verify(memberRepository).delete(any());
     }
 }
