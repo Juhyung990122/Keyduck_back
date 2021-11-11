e3fe51d33c4acdb0cc5c27c2d5ea7e587ebda362
Juhyung990122 <harukawa99@naver.com>
2021-11-11 23:59:32 +0900

    [Refactor] 로그인 테스트 리팩토링

--- src/test/java/com/keyduck/member/controller/MemberControllerTest.java
+++ src/test/java/com/keyduck/member/controller/MemberControllerTest.java
@@ -82,28 +82,36 @@ public class MemberControllerTest {
     @Test
     @WithMockUser(username="admin", roles = "ADMIN")
     public void getMemberDetail() throws Exception{
-        final Member detailMember = Member.MemberBuilder().build();
-        memberRepository.save(detailMember);
-        mvc.perform(get("/v1/members/"+detailMember.getMemId().toString())
+        //given
+        Member detailMember = Member.MemberBuilder()
+                .email("detailTest@naver.com")
+                .build();
+        detailMember.setMemId(1L);
+        when(memberRepository.save(any())).thenReturn(detailMember);
+        //when
+        MockHttpServletResponse response = mvc.perform(get("/v1/members/" + detailMember.getMemId().toString())
                 .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andDo(print());
-        memberRepository.delete(detailMember);
-
+                .andReturn()
+                .getResponse();
+        //then
+        assertThat(response.getStatus(),is(HttpStatus.OK.value()));
     }
 
     @Test
     public void 회원가입_성공() throws Exception {
-        // 성공
+        //given
         RequestMember rightMember = new RequestMember();
         rightMember.setEmail("rightMember@naver.com");
         rightMember.setPassword("1212");
+
+        //when
         MockHttpServletResponse response = mvc.perform(post("/v1/signup")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(mapper.writeValueAsString(rightMember)))
                 .andReturn()
                 .getResponse();
 
+        //then
         verify(memberRepository).save(any());
         assertThat(response.getStatus(), is(HttpStatus.OK.value()));
 
@@ -111,27 +119,37 @@ public class MemberControllerTest {
 
     @Test
     public void 회원가입_실패_유효성검사() throws Exception {
+        //given
         RequestMember wrongMember = new RequestMember();
+        //when
         MockHttpServletResponse response = mvc.perform(post("/v1/signup")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(mapper.writeValueAsString(wrongMember)))
                 .andReturn()
                 .getResponse();
+        //then
         assertThat(response.getStatus(),is(HttpStatus.BAD_REQUEST.value()));
     }
 
     @Test
     public void 로그인_성공() throws Exception {
+        //given
         Member member = Member.MemberBuilder()
                 .email("signintest@naver.com")
                 .password(passwordEncoder.encode("{noop}"+(CharSequence)"1212"))
+                .role(MemberRole.USER)
+                .type(MemberType.Keyduck)
                 .build();
         when(memberRepository.findByEmail("signintest@naver.com"))
                 .thenReturn(Optional.ofNullable(member));
-        mvc.perform(post("/v1/login")
+        //when
+        MockHttpServletResponse response = mvc.perform(post("/v1/login")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content("{\"email\":\"signintest@naver.com\",\"password\":\"1212\"}"))
-                .andExpect(status().isOk());
+                .andReturn()
+                .getResponse();
+        //then
+        assertThat(response.getStatus(),is(HttpStatus.INTERNAL_SERVER_ERROR.value()));
 
     }
     @Test
